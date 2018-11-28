edade111dd526f17f2d5be04f044cb1ac703bbeb
roebinchristie <44249273+roebinchristie@users.noreply.github.com>
2018-11-27 20:01:37 -0600

    Add files via upload

---
+++ unit 7 lab.ps1
@@ -0,0 +1,16 @@
+﻿$user = read-host "Do you want to exit?"
+$user
+$server = Get-Content serverlist.csv
+
+while ($user -eq "no"){
+    Set-Location C:\
+    $Jobs = Import-Csv employeelist.csv 
+        ForEach ($Job in $Jobs){
+            New-Item -Name $Job.Name.Replace(' ','').ToLower() -Type directory -Path C:\fileserver
+            get-job
+            New-Item -Name Job.txt -Type file -Value "Welcome, $($Job.Name)!!!!?" -Path C:\fileserver\$($Job.Name.Replace(' ','').ToLower())
+        }
+    ForEach ($server in $servers){
+        Test-Connection -ComputerName $server -Quiet
+    }
+}
